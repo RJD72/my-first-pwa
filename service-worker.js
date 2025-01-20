6b4c4999d2e3bab6293600e134d0362dbdfa9429
robdeer1 <r_deer@fanshaweonline.ca>
2025-01-20 13:05:07 -0500

    updated service worker file with repository name

--- service-worker.js
+++ service-worker.js
@@ -1,12 +1,12 @@
 const CACHE_NAME = "to-do-pwa-cache-v1";
 const FILES_TO_CACHE = [
-  "/",
-  "/index.html",
-  "/style.css",
-  "/app.js",
-  "/manifest.json",
-  "/icons/icon-128.png",
-  "/icons/icon-512.png",
+  "/my-first-pwa",
+  "/my-first-pwa/index.html",
+  "/my-first-pwa/style.css",
+  "/my-first-pwa/app.js",
+  "/my-first-pwa/manifest.json",
+  "/my-first-pwa/icons/icon-128.png",
+  "/my-first-pwa/icons/icon-512.png",
 ];
 
 self.addEventListener("install", (event) => {
